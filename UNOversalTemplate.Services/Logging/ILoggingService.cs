5a23bd6057c42eaa3411fc65d4f745ae72e84b5b
Jan Philipp Weber <10946850+jp-weber@users.noreply.github.com>
2024-06-16 11:11:54 +0200

    feat: add logging service

---
+++ UNOversalTemplate.Services/Logging/ILoggingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UNOversal.Services.Logging
+{
+    public interface ILoggingService
+    {
+        Task Log(string message, LoggingPreferEnum loggingPreferEnum);
+        Task LogException(Exception exc, LoggingPreferEnum loggingPreferEnum);
+    }
+}
---
+++ UNOversalTemplate.Services/Logging/LoggingPreferEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace UNOversal.Services.Logging
+{
+    public enum LoggingPreferEnum
+    {
+        None = 0,
+        Simple = 1,
+        Full = 2
+    }
+}
